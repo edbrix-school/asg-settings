5a48f223d9d0792acd603a48de7723cce64a96f6
Gouri Sanjay Jadhav <gourij@hexalytics.com>
2026-01-11 15:14:09 +0530

    fixed address master delete issue

--- src/main/java/com/asg/settings/repository/AddressMasterRepository.java
+++ src/main/java/com/asg/settings/repository/AddressMasterRepository.java
@@ -3,12 +3,17 @@ package com.asg.settings.repository;
 import com.asg.settings.entity.AddressMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressMasterRepository extends JpaRepository<AddressMaster, Long>, JpaSpecificationExecutor<AddressMaster> {
 
     AddressMaster findByAddressMasterPoid(Long addressMasterPoid);
+
+    @Query("SELECT a FROM AddressMaster a WHERE a.addressMasterPoid = :addressMasterPoid AND a.active = 'Y' AND a.deleted = 'N'")
+    AddressMaster findActiveByAddressMasterPoid(@Param("addressMasterPoid") Long addressMasterPoid);
 
     boolean existsByAddressMasterPoid(Long addressMasterPoid);
 
