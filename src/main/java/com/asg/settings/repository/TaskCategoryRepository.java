9ca83a8f43923c9fbc2300b9284d4dfae75d7243
Gouri Sanjay Jadhav <gourij@hexalytics.com>
2026-01-11 14:48:45 +0530

    fixed task category delete issue

--- src/main/java/com/asg/settings/repository/TaskCategoryRepository.java
+++ src/main/java/com/asg/settings/repository/TaskCategoryRepository.java
@@ -3,11 +3,16 @@ package com.asg.settings.repository;
 import com.asg.settings.entity.TaskCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskCategoryRepository extends JpaRepository<TaskCategoryEntity, Integer>, JpaSpecificationExecutor<TaskCategoryEntity> {
     TaskCategoryEntity findByCategoryPoid(Long categoryPoid);
+
+    @Query("SELECT t FROM TaskCategoryEntity t WHERE t.categoryPoid = :categoryPoid AND t.active = 'Y' AND t.deleted = 'N'")
+    TaskCategoryEntity findActiveByCategoryPoid(@Param("categoryPoid") Long categoryPoid);
 
     boolean existsByCategoryDescriptionIgnoreCase(String categoryDescription);
 
