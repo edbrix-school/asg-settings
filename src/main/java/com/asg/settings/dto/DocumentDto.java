8809c7b5fbe36f275a0607e33256fc589dfed6a2
Gouri Sanjay Jadhav <gourij@hexalytics.com>
2026-02-17 18:04:29 +0530

    removed json ignore from list of records sql

--- src/main/java/com/asg/settings/dto/DocumentDto.java
+++ src/main/java/com/asg/settings/dto/DocumentDto.java
@@ -57,7 +57,6 @@ public class DocumentDto {
     private String mainTableName;
     private String editableOnSameDay;
     private String hideInMainMenu;
-    @com.fasterxml.jackson.annotation.JsonIgnore
     private String listOfRecordsSql;
     private String docKeyField;
     private String docReturnFields;
