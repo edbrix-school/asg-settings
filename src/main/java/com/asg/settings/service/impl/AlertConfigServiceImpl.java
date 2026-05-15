102b34ca8a18af300292933854639fc29566086b
Gouri Sanjay Jadhav <gourij@hexalytics.com>
2026-05-15 11:04:31 +0530

    removed group poid from alert and config

--- src/main/java/com/asg/settings/service/impl/AlertConfigServiceImpl.java
+++ src/main/java/com/asg/settings/service/impl/AlertConfigServiceImpl.java
@@ -62,7 +62,6 @@ public class AlertConfigServiceImpl implements AlertConfigService {
     public AlertAndRemainderDto createAlert(AlertAndRemainderDto request) {
 
         AlertConfigEntity entity = convertFromAlertDtoToAlertEntity(request);
-        entity.setGroupPoid(UserContext.getGroupPoid());
         AlertConfigEntity alertConfigEntity = alertConfigRepository.save(entity);
         String docId = UserContext.getDocumentId();
         String key = alertConfigEntity.getConfigPoid().toString();
